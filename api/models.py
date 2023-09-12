4e6d1ba49d2f9a4787b212a1721954ef5fc3ad83
Beloved-vince <vinceoludare@gmail.com>
2023-09-12 12:54:47 +0100

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    dob = models.DateField(null=False, blank=False)
+    gender = models.CharField(max_length=1)
+    
+    
