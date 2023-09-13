5621ef08b82e2be6f67a4fefcd6e6e1bf56996ee
Beloved-vince <vinceoludare@gmail.com>
2023-09-13 15:38:07 +0100

    update api

--- CRUD_API/settings.py
+++ CRUD_API/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'CRUD_API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crud',
-        'USER': 'root',
-        'PASSWORD': 'Oludare2001',
-        'HOST':'localhost',
-        'PORT':'3306',
-        'OPTIONS':{
-            'autocommit':True
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'crud',
+#         'USER': 'root',
+#         'PASSWORD': 'Oludare2001',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#         'OPTIONS':{
+#             'autocommit':True
+#         }
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# hng-task-stage_2
+# Project Name
+
+A brief description of your Django project.
+
+## Table of Contents
+
+- [Installation](#installation)
+  - [Prerequisites](#prerequisites)
+  - [Environment Setup](#environment-setup)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python: This project is built using Python. You can download it from [python.org](https://www.python.org/downloads/).
+
+### Environment Setup
+
+1. **Clone the repository to your local machine:**
+
+   ```bash
+   git clone https://github.com/your-username/your-project.git
